d069021a0e49908d41c45b01795d013288077470
boatware <thwohmas@gmail.com>
2022-05-31 15:06:55 +0200

    Added global configuration

--- setup/main.go
+++ setup/main.go
@@ -1,13 +1,34 @@
 package setup
 
 import (
+	"encoding/json"
 	"github.com/joho/godotenv"
+	"io/ioutil"
 	"kulana/misc"
 	"os"
 )
 
+type Config struct {
+	Mail MailConfig `json:"mail"`
+}
+
+type MailConfig struct {
+	StatusCodes  string `json:"status_codes"`
+	Subject      string `json:"subject"`
+	TemplateFile string `json:"template_file"`
+}
+
 var setupDir = os.Getenv("HOME") + "/.kulana"
 var envFile = setupDir + "/.env"
+var configFile = setupDir + "/config.json"
+
+var defaultConfig = Config{
+	Mail: MailConfig{
+		StatusCodes:  "4xx,5xx",
+		Subject:      "Host %s is %s",
+		TemplateFile: setupDir + "/mail.html",
+	},
+}
 
 func getSetupDir() string {
 	return setupDir
@@ -18,6 +39,38 @@ func GetEnvFile() string {
 }
 
 func EnsureEnvironmentIsReady() {
+	createSetupDirIfNotExists()
+	ensureEnvFileIsReady()
+	ensureConfigFileIsReady()
+}
+
+func ensureConfigFileIsReady() {
+	_, err := os.Stat(configFile)
+	if os.IsNotExist(err) {
+		defaultConfig := getDefaultConfig()
+		defaultConfigJson, jErr := json.Marshal(defaultConfig)
+		misc.Check(jErr)
+
+		fErr := os.WriteFile(configFile, defaultConfigJson, 0644)
+		misc.Check(fErr)
+	}
+}
+
+func ensureEnvFileIsReady() {
+	// Check if environment file ~/.kulana/.env exists
+	_, err := os.Stat(envFile)
+	if os.IsNotExist(err) {
+		defaultEnv := getDefaultEnv()
+
+		fErr := os.WriteFile(envFile, defaultEnv, 0644)
+		misc.Check(fErr)
+	}
+
+	err = godotenv.Load(envFile)
+	misc.Check(err)
+}
+
+func createSetupDirIfNotExists() {
 	// Check if dir ~/.kulana exists
 	_, err := os.Stat(setupDir)
 	if os.IsNotExist(err) {
@@ -28,22 +81,41 @@ func EnsureEnvironmentIsReady() {
 	} else if err != nil {
 		panic(err)
 	}
+}
 
-	// Check if environment file ~/.kulana/.env exists
-	_, err = os.Stat(envFile)
+func getDefaultEnv() []byte {
+	return []byte("" +
+		"SMTP_HOST=\n" +
+		"SMTP_USERNAME=\n" +
+		"SMTP_PASSWORD=\n" +
+		"SMTP_PORT=\n" +
+		"SMTP_ADDRESS=\n" +
+		"")
+}
+
+func getDefaultConfig() Config {
+	return defaultConfig
+}
+
+func ReadConfig() Config {
+	_, err := os.Stat(configFile)
 	if os.IsNotExist(err) {
-		defaultEnv := []byte("" +
-			"SMTP_HOST=\n" +
-			"SMTP_USERNAME=\n" +
-			"SMTP_PASSWORD=\n" +
-			"SMTP_PORT=\n" +
-			"SMTP_ADDRESS=\n" +
-			"")
-
-		fErr := os.WriteFile(envFile, defaultEnv, 0644)
-		misc.Check(fErr)
+		// Config file should exist at this point.
+		panic(err)
 	}
 
-	err = godotenv.Load(envFile)
-	misc.Check(err)
+	configJsonFile, fErr := os.Open(configFile)
+	misc.Check(fErr)
+	defer func(configJsonFile *os.File) {
+		err := configJsonFile.Close()
+		misc.Check(err)
+	}(configJsonFile)
+
+	byteValue, _ := ioutil.ReadAll(configJsonFile)
+	var config Config
+
+	jErr := json.Unmarshal(byteValue, &config)
+	misc.Check(jErr)
+
+	return config
 }
